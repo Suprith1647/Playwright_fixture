299e5461519bfcb10ba61b6bc325d433807c3e12
Suprith1647 <82437324+Suprith1647@users.noreply.github.com>
2023-04-12 18:44:03 +0530

    addcart

---
+++ pages/homePage.ts
@@ -0,0 +1,83 @@
+import { expect, Locator, Page } from "@playwright/test";
+import constant from "../utils/lamdatest.json"; 
+
+
+export class HomePage {
+    readonly page: Page;
+    readonly homePageLocators:any;
+    readonly billingAddressLocators:any;
+
+
+
+    constructor(page: Page) {
+        this.page = page;
+        this.homePageLocators={
+            
+            home_link : "//span[text()[normalize-space()='Home']]",
+
+        }
+        this.billingAddressLocators={
+               
+            firstname:"#input-payment-firstname",
+            lastname :"#input-payment-lastname",
+            company : "#input-payment-company",
+            address_1: "#input-payment-address-1",
+            address_2: "#input-payment-address-2",
+            city     : "#input-payment-city",
+            post_code : "#input-payment-postcode",
+            country  : "#input-payment-country",
+            region :"#input-payment-zone",
+            tearm_condition : `input[name='agree']`,
+            continue_Btn  : "//button[text()='Continue ']"
+
+        }
+
+    }
+    async clickHomeMenu(){
+    await this.page.locator(this.homePageLocators.home_link).click();
+    await expect(this.page).toHaveURL(/.*home/);
+    await this.page.waitForTimeout(5000);
+    await this.page.hover("//div[@class='image']/a",{strict:false});
+    await this.page.locator("(//button[@title='Add to Cart'])").nth(0).click();
+          
+    }
+    async isTostVisible(){
+        const toast = this.page.locator("//a[.='View Cart ']");
+        await toast.waitFor({state:"visible"})
+        return toast; 
+    }
+
+      async addCart(){
+      await this.page.locator("//a[contains(text(),'View Cart')]").click();
+      await expect(this.page).toHaveURL(/.*cart/); 
+      }
+
+      async checkOut(){
+        await this.page.locator("//a[contains(text(),'Checkout')]").click();
+        await expect(this.page).toHaveURL(/.*checkout/);      
+
+      }
+      async fillBillingAddress(){
+        await this.page.locator(this.billingAddressLocators.firstname).type(constant.Fname);
+        await this.page.locator(this.billingAddressLocators.lastname).type(constant.Lname);
+        await this.page.locator(this.billingAddressLocators.company).type(constant.Company);
+        await this.page.locator(this.billingAddressLocators.address_1).type(constant.address_1);
+        await this.page.locator(this.billingAddressLocators.address_2).type(constant.address_2);
+        await this.page.locator(this.billingAddressLocators.city).type(constant.city);
+        await this.page.locator(this.billingAddressLocators.post_code).type(constant.postcode);
+        await this.page.locator(this.billingAddressLocators.country).selectOption('India');
+        await this.page.locator(this.billingAddressLocators.region).selectOption('Karnataka');
+        await this.page.locator(this.billingAddressLocators.tearm_condition).click({force:true});
+        await this.page.locator(this.billingAddressLocators.continue_Btn).click();
+
+
+      
+
+      }
+    }
+    
+
+   
+    
+   
+
--- tests/lambdAddcart.spec.ts
+++ tests/lambdAddcart.spec.ts
@@ -1,12 +1,14 @@
 import { expect, test, Page } from "@playwright/test";
 import { RegistrationPage } from "./../pages/registrationPage";
 import { LoginPage } from "./../pages/loginPage";
+import { HomePage } from "../pages/homePage";
 import {Support} from "./../utils/constant";
 
 let page: Page;
 let browser, context: any;
 let registrationPage : RegistrationPage;
 let loginPage : LoginPage;
+let homePage : HomePage;
 let support: Support;
 
 test.beforeEach(async ({ browser }) => {
@@ -14,6 +16,7 @@ test.beforeEach(async ({ browser }) => {
     page = await context.newPage();
     registrationPage = new RegistrationPage(page);
     loginPage   = new LoginPage(page);
+    homePage = new HomePage(page);
     support = new Support();
 
 })
@@ -28,8 +31,14 @@ test("Login test", async({baseURL}) =>{
     await page.goto(`${baseURL}route=account/login`);
     await loginPage.loginFillforms();
     await expect(page).toHaveURL(support.loginConfim);
-    
-})
+    await homePage.clickHomeMenu();
+    await homePage.isTostVisible();
+    await homePage.addCart();
+    await homePage.checkOut();   
+    await homePage.fillBillingAddress(); 
+
+});
+
 
 test.afterAll(async (browser) => {
     await page.close();
